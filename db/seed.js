a5db0237e475c226c4ff33b7d2b7a0d3701cf668
pmxperiment <lenstesterguy@gmail.com>
2018-02-27 09:28:38 -0800

    app.js package json

--- db/seed.js
+++ db/seed.js
@@ -5,7 +5,7 @@ var models = require('../models');
   {
    set_name:'Penthouse',
    category: 'Homes & Residences',
-   daily_fee: '$300',
+   daily_fee: ' $300',
    pic_url: 'https://static.mansionglobal.com/production/media/article-images/8bf4b58342b20c211dc28b2b3c9422a3/small_MN-AQ624_MAGNUM_M_20180130133401.jpg',
    description: 'Penthouse located on the 53rd floor of the Market Street Tower Residences. Great for dialog shots and also panoramic establishing shots of SF.'
   },
@@ -14,7 +14,7 @@ var models = require('../models');
     category: 'Homes & Residences',
     daily_fee: '$100',
     pic_url: 'http://www.sanluisobispo.com/news/local/community/cambrian/bqggnl/picture165556052/alternates/FREE_480/Neptune%20Pool064',
-    description: 'Pool with decor aestetics resembling ancient Greece.'
+    description: 'Pool with decor aesthetics resembling ancient Greece.'
   },
   {
     set_name: 'Rooftop',
@@ -54,39 +54,38 @@ models.Location.remove({}, function(err, res) {
   }
   console.log('Removed all Locations');
 
-  models.Location.create(LocationsList, function(err, comments) {
+  models.Location.create(LocationsList, function(err, locations) {
     if (err) {
       console.log('Error creating locations: ', err);
       return;
     }
-    console.log('Created', Location.length, 'Location');
-    return;
+    console.log('Created', locations.length, 'Location');
+
+    models.User.remove({}, function(err, res) {
+      if (err) {
+        console.log('Error removing Users', err);
+        return;
+      }
+      console.log('Removed all Users');
+
+
+    models.User.create([
+    {
+       name: 'Tom',
+       locations: [locations[0], locations[1], locations[2]],
+     },
+    {
+       name: 'Sam',
+     locations: [locations[3], locations[4], locations[5]]
+    }], function(err, users) {
+      if (err) {
+        console.log('Error creating Users', err);
+        return;
+      }
+      console.log('Created', users.length, 'users');
+      process.exit();
+    });
+    });
+
   })
-});
-
-
-models.User.remove({}, function(err, res) {
-  if (err) {
-    console.log('Error removing Users', err);
-    return;
-  }
-  console.log('Removed all Users');
-
-
-models.User.create([
-{
-   name: 'Tom',
-   locations: [LocationsList[0], LocationsList[1], LocationsList[2]],
- },
-{
-   name: 'Sam',
- locations: [LocationsList[3], LocationsList[4], LocationsList[5]]
-}], function(err, users) {
-  if (err) {
-    console.log('Error creating Users', err);
-    return;
-  }
-  console.log('Created', users.legnth, 'users');
-  process.exit();
-});
 });
