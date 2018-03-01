48d34fe4e61e6630ee5f7ec9c78154663a9fe3ee
pmxperiment <lenstesterguy@gmail.com>
2018-02-28 22:44:56 -0800

    testing for hosted deplyment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:/8080", {useMongoClient: true});
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:/8080");
 
 module.exports.User = require("./User");
 module.exports.Location = require("./Location");
