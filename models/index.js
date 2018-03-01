fc64c60b6797d0329b700f420b4fac05a9b4c144
pmxperiment <lenstesterguy@gmail.com>
2018-03-01 09:11:48 -0800

    altered mongodb uri again part 2

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:/8080");
+//mongoose.connect("mongodb://localhost:/8080");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:/8080");
 
 
 module.exports.User = require("./User");
