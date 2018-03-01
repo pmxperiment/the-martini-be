a4d41fa1b75c09a15e005b5e8692a0d0c248eaca
pmxperiment <lenstesterguy@gmail.com>
2018-02-28 23:54:36 -0800

    testing

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:/8080");
+mongoose.connect("mongodb://localhost:/8080");
+
 
 module.exports.User = require("./User");
 module.exports.Location = require("./Location");
