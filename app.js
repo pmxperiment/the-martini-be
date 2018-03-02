a0c46ad43c3ee946d8125e12e06f836182e6eb23
pmxperiment <lenstesterguy@gmail.com>
2018-03-02 13:25:21 -0800

    cleaned up commented out code in BE

--- app.js
+++ app.js
@@ -9,17 +9,6 @@ var path = require('path');
 var user = require('./models/User')
 
 
-
-
-//old cors
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "PUT,GET,POST,DELETE,OPTIONS");
-//   next();
-// });
-
-//new cors
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -38,7 +27,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-
 app.options('/*', function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -47,30 +35,7 @@ app.options('/*', function(req, res, next){
 });
 
 
-
-
-
-
-
-
-
-//bodyParser middleware
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(router);
-
-// test data
-// var test1 = {
-//   name: 'Tom',
-//   location: 'pool'
-// };
-//
-//
-// //test routes
-// app.get('/api/users', function(req, res){
-//   res.json(test1);
-// });
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('Express server is up and running on https://localhost:8080/');
